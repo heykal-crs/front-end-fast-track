e2c02a922a275cb6304bee8d288f82d7e5805b05
heykal-crs <heykalcrs@gmail.com>
2022-09-06 12:38:01 +0700

    add accordian

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const hideModalButton = document.querySelectorAll('#cancelBtn, #modalHeader');
 const modalTitle = document.getElementById('modalTitle');
 const modalImage = document.getElementById('image-modal');
 
+/* Accordian variables */
+const accordianButton = document.getElementsByClassName('accBtn');
 
-
-function validateForm () {
+/* Show modal */
+showModalButton.addEventListener('click', () =>{
 	const emailBox = document.forms['form1']['email'].value;
 	const msgBox = document.forms['form1']['text-msg'].value;
-
 	if (emailBox == '' && msgBox == ''){
 		alert('Each box has to be filled out');
 		return false;
@@ -23,20 +24,18 @@ function validateForm () {
 	else if (msgBox == ''){
 		alert('Message box has to be filled out');
 		return false;
+	} else{
+		modal.style.display = 'block';
+		buttonSubmit.style.display = 'block';
+		modalImage.src = './img/ninja.png';
+		modalTitle.innerHTML = 'Mengirim Pesan?';
+		hideModalButton[1].innerHTML = "<b>Cancel</b>";
+		animated(modal, 200, 0.5, 'topDown');
+
+		changeModalAttribure();
 	}
-	changeModalAttribure();
-}
-
-/* Show modal */
-showModalButton.addEventListener('click', () =>{
-	modal.style.display = 'block';
-	buttonSubmit.style.display = 'block';
-	modalImage.src = './img/ninja.png';
-	modalTitle.innerHTML = 'Mengirim Pesan?';
-	hideModalButton[1].innerHTML = "<b>Cancel</b>";
-	animatedModal();
+	
 });
-
 
 function changeModalAttribure(){
 	buttonSubmit.addEventListener('click', function (){
@@ -56,17 +55,37 @@ for (let i = 0; i < hideModalButton.length; i++){
 };
 
 
+/* Accordian */
+for (let i = 0; i < accordianButton.length; i++){
+	accordianButton[i].addEventListener('click', function(){
+		const panel = this.nextElementSibling;
 
-/* animated modal */
-function animatedModal () {
-	let time = setInterval(frame, 0.5);
+		if (panel.style.display === 'block'){
+			panel.style.display = 'none';
+		} else{
+			panel.style.display = 'block';
+			animated(panel, 1, 5, 'show');
+		}
+	})
+}
+
+
+/* animated */
+function animated(element, top, times, type) {
+	let time = setInterval(frame, times);
 	let pos = 0;
 	function frame(){
-		if (pos == 200){
+		if (pos == top){
 			clearInterval(time);
 		} else{
-			pos+=2;
-			modal.style.paddingTop = pos + "px";
+			if (type == 'topDown'){
+				pos+=2;
+				element.style.paddingTop = pos + "px";
+			}
+			else if (type == 'show'){
+				pos+=0.1;
+				element.style.opacity = pos;
+			}
 		}
 	}
 }
